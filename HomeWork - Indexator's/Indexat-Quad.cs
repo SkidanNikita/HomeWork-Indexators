957b5bfcd0d0e534ec3ccc5804e659b2e834d134
Ник Скидан <skidannikitas@gmail.com>
2020-06-29 19:29:37 +0600

    HomeWork with indexator's

---
+++ HomeWork - Indexator's/Indexat-Quad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork___Indexator_s
+{
+    public class Indexat_Quad
+    {
+        private int[] number = new int[100];
+        public int this[int index]
+        {
+            get
+            {
+                return number[index];
+            }
+            set
+            {
+                number[index] = value * value;
+            }
+        }
+    }
+}
---
+++ HomeWork - Indexator's/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork___Indexator_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write("Please, enter ");Console.Write(i + 1);Console.WriteLine(" number");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i); 
+            }
+        }
+    }
+}
